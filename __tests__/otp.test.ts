d1d27ac3d2044f6655fcae773fb775f96861bdcf
andR3Scr1pTx86 <189578110+andR3Scr1pTx86@users.noreply.github.com>
2025-10-22 22:35:21 -0300

    adding one more expect ( be string )

--- __tests__/otp.test.ts
+++ __tests__/otp.test.ts
@@ -2,8 +2,9 @@ import { generate } from '../src/functions';
 
 describe('generate', () => {
     it('should create a simple otp code only with numbers', () => {
-        const result = generate({ length: 6 })
+        const result = generate()
 
+        expect(typeof result).toBe('string')
         expect(result).toMatch(/^\d+$/)
         expect(result).toHaveLength(6)
     })
