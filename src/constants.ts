d53be637830586f387a45cc73360f6cd1ca8e2d7
andR3Scr1pTx86 <189578110+andR3Scr1pTx86@users.noreply.github.com>
2025-10-25 00:23:34 -0300

    creating constants file to not populate functions file

---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+export const MIN_LENGTH: number = 4;
+export const MAX_LENGTH: number = 10;
+
+export const NUMERIC: string = "0123456789"
+export const LOWER_ALPHABET: string = "abcdefghijklmnopqrstuvwxyz"
+export const UPPER_ALPHABET: string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+export const SPECIAL_CHARS: string = "!@#$%&*"
