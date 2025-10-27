d7223bf6b62608ffda81816aeee678ac10ef0f82
andR3Scr1pTx86 <189578110+andR3Scr1pTx86@users.noreply.github.com>
2025-10-27 00:13:56 -0300

    updating tests imported main func

--- __tests__/otp.test.ts
+++ __tests__/otp.test.ts
@@ -1,4 +1,4 @@
-import { generate } from '../src/functions';
+import { generateOTP } from '../src/functions';
 
 import { MIN_LENGTH, MAX_LENGTH } from '../src/constants'
 
@@ -11,7 +11,7 @@ describe('generate', () => {
         let foundSpecialChar: boolean = false;
 
         for (let i = 0; i < 20; i++) {
-            const otp = generate(10, {
+            const otp = generateOTP(10, {
                 numeric: true,
                 lowerAlphabet: true,
                 upperAlphabet: true,
@@ -35,7 +35,7 @@ describe('generate', () => {
     });
 
     it('should be able to generate only upper alphabet and numbers characters', () => {
-        expect(generate(10, {
+        expect(generateOTP(10, {
             numeric: false,
             lowerAlphabet: false,
             upperAlphabet: false,
@@ -44,7 +44,16 @@ describe('generate', () => {
     })
 
     it('should be able to generate only numbers characters', () => {
-        expect(generate(10, {
+        const test = generateOTP(10, {
+            numeric: true,
+            lowerAlphabet: true,
+            upperAlphabet: true,
+            specialChar: false
+        })
+
+        console.log(test)
+
+        expect(generateOTP(10, {
             numeric: true,
             lowerAlphabet: false,
             upperAlphabet: false,
@@ -53,22 +62,22 @@ describe('generate', () => {
     })
 
     it('should throw an error if the length is a decimal number', () => {
-        expect(() => generate(4.2)).toThrow('OTP length must be a positive integer');
+        expect(() => generateOTP(4.2)).toThrow('OTP length must be a positive integer');
     });
 
     it('should throw an error if the length is zero', () => {
-        expect(() => generate(0)).toThrow('OTP length must be a positive integer');
+        expect(() => generateOTP(0)).toThrow('OTP length must be a positive integer');
     });
 
     it('should throw an error if the length is a negative number.', () => {
-        expect(() => generate(-10)).toThrow('OTP length must be a positive integer');
+        expect(() => generateOTP(-10)).toThrow('OTP length must be a positive integer');
     });
 
     it('should throw an error if the length is less than the minimum allowed', () => {
-        expect(() => generate(MIN_LENGTH - 1)).toThrow(`OTP length must be between ${MIN_LENGTH} and ${MAX_LENGTH}`);
+        expect(() => generateOTP(MIN_LENGTH - 1)).toThrow(`OTP length must be between ${MIN_LENGTH} and ${MAX_LENGTH}`);
     });
 
     it('should throw an error if the length is greater than the maximum allowed', () => {
-        expect(() => generate(MAX_LENGTH + 1)).toThrow(`OTP length must be between ${MIN_LENGTH} and ${MAX_LENGTH}`);
+        expect(() => generateOTP(MAX_LENGTH + 1)).toThrow(`OTP length must be between ${MIN_LENGTH} and ${MAX_LENGTH}`);
     });
 })
